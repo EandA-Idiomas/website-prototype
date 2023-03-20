71a182d034612610ce60ee6bfb71a6ae5641aa00
pdragoni <paulohbfdf@gmail.com>
2023-03-20 13:27:33 -0300

    [fix] methodology component with correct content and divs

--- eanda-frontend/src/Pages/Methodology.jsx
+++ eanda-frontend/src/Pages/Methodology.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from "../Components/Header";
-import ArticleBody from '../Components/Article__';
+import methdology_content from "../Components/methodology_components/methodology";
 import Footer from "../Components/Footer";
 
 
 function Methodology() {
+  const { title, content, valores, image, matcher } = methdology_content;
+
   return (
     <div>
       <Header />
-      <ArticleBody />
+      {
+        <div className="article-container">
+          <h3>{title}</h3>
+          {content.map((content) => (
+            <p>{content}</p>
+          ))}
+
+        </div>
+      }
+      <Link to="/"> Back to Home Page</Link>
       <Footer />
-    </div>
+    </div >
   );
 }
 
