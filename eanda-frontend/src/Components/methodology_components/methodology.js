990b167a19e1c026eef3d027531e77619799533c
pdragoni <paulohbfdf@gmail.com>
2023-03-20 13:26:42 -0300

    [fix] methodology content

---
+++ eanda-frontend/src/Components/methodology_components/methodology.js
@@ -0,0 +1,31 @@
+import metodologia_img from "./3D_Open_Book.png";
+
+const metodologia = ` Construtivismo - “Common European Frame Work”
+
+O principal objetivo da “abordagem comunicativa” no aprendizado de um segundo idioma é alterar o foco da aprendizagem; dentro da educação tradicional sempre se focou, principalmente, na acuidade da gramática, deixando em segundo plano a comunicação em si. Em suma, aprendizes de uma segunda língua eram experts em verificação, correção e tradução da língua, mas, em grande maioria, se sentiam incapazes de se comunicar na língua alvo.
+
+A abordagem comunicativa apresenta um conjunto de técnicas a serem adotadas pelo professor, as quais possibilitam a condução da aula onde seu papel passa a ser de um mediador, deixando de ser o foco maior, passando a intermediar o andamento das atividades. Para isso, é necessário que o profissional esteja preparado e vigilante às necessidades de cada aluno — que serão as mais diversas — para que, assim, seja possível estabelecer um ambiente criativo e participativo. Em outras palavras, a preparação da aula se baseia, com muito mais peso, nas necessidades, interesses e anseios do aluno.
+
+Conclui-se o material de onboarding agradecendo sua participação e interesse em fazer parte dessa equipe. Somos um grupo extremamente motivado a superar as barreiras técnicas e comuns existentes em todas as escolas de inglês, oferecendo algo muito maior que ensinar-se uma língua. Utilizando a abordagem comunicativa, o aprendizado ocorre naturalmente por meio do estudo de temas relacionados à realidade do aluno.
+
+Assim, trazemos mais relevância ao aproximar o idioma do dia-a-dia do aluno. Temas e situações de negócios são abrodados em vários níveis e atendendo diferentes necessidades.
+
+Programas flexíveis que atendem a níveis específicos de necessidades e se adaptam ao ritmo e velocidade do aluno.`;
+
+const plano_de_carreira = `Ao observar comprometimento, criatividade e, principalmente, desejo de crescer, a escola propões atividades extras com pagamento à parte que envolverão muito além de lecionar.`;
+
+const responsabilidades = `Trabalhar com produção de materiais, produção de vídeos, aulas online ou outras mídias, fazer parte da equipe andragógica e de treinamento. Salário hora-aula competitivo, treinamento pago, o aumento de salário é baseado na obtenção de KPIs [Key Performance Indicators].`;
+
+const treinamentos_e_workshops = `A E and A Idiomas preza pelo constante desenvolvimento e aprimoramento de sua equipe, oferecendo treinamentos e workshops recorrentes e remunerados, focados em metodologia, didática, procedimentos, entre outros. O objetivo é evoluir, crescer como profissionais e oferecer uma entrega acima das expectativas de nossos clientes.
+
+Por conseguinte, é notória a necessidade de que todos os colaboradores da E and A Idiomas participem dessas atividades; estas não são obrigatórias, mas irão definir decisivamente na continuidade da parceria entre a escola e o professor.`;
+
+const methodology_content = {
+  id: 2,
+  title: ["Nossa Metodologia"],
+  content: [metodologia, plano_de_carreira, responsabilidades, treinamentos_e_workshops],
+  image: metodologia_img,
+  matcher: "/methodology",
+};
+
+export default methodology_content;
