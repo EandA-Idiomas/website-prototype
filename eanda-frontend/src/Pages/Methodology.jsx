7f49167653f138e3288d0ae7c13693cf7757680f
pdragoni <paulohbfdf@gmail.com>
2023-03-21 13:12:25 -0300

    [fix] Methodology Page verification through useLocation

--- eanda-frontend/src/Pages/Methodology.jsx
+++ eanda-frontend/src/Pages/Methodology.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import Header from "../Components/Header";
 import methdology_content from "../Components/methodology_components/methodology";
 import Footer from "../Components/Footer";
 
 
 function Methodology() {
-  const { title, content, valores, image, matcher } = methdology_content;
+  const location = useLocation();
+  const currentUrl = location.pathname;
+
+  const { title, content, image, matcher } = methdology_content;
 
   return (
     <div>
       <Header />
       {
-        <div className="article-container">
-          <h3>{title}</h3>
-          {content.map((content) => (
-            <p>{content}</p>
-          ))}
-
-        </div>
-      }
-      <Link to="/"> Back to Home Page</Link>
+        matcher === currentUrl && (
+          <body>
+            <section className="article-container">
+              <h3>{title}</h3>
+              {content.map((content) => (
+                <p>{content}</p>
+              ))}
+            </section>
+            <Link to="/"> Back to Home Page</Link>
+          </body>
+        )}
+      <img src={image} alt={matcher} />
       <Footer />
     </div >
   );
