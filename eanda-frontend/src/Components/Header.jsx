ca7504add789d356e615fbd0d63e08acd785af77
pdragoni <paulohbfdf@gmail.com>
2023-03-21 19:39:36 -0300

    Fix link names at Header.jsx

--- eanda-frontend/src/Components/Header.jsx
+++ eanda-frontend/src/Components/Header.jsx
@@ -25,9 +25,9 @@ function Header() {
       </div>
       <nav>
         <ul className="links">
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="contact">Contact</a></li>
+          <li><a href="/about">Sobre Nós</a></li>
+          <li><a href="/services">Serviços</a></li>
+          <li><a href="contact">Contato</a></li>
         </ul>
       </nav>
     </header>
