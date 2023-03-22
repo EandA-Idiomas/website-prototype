935d3529549385ddca597f85dd382df002d43f4b
pdragoni <paulohbfdf@gmail.com>
2023-03-22 18:43:59 -0300

    [fix] SliderData with only two objects

--- eanda-frontend/src/Components/slider_components/SliderData.js
+++ eanda-frontend/src/Components/slider_components/SliderData.js
@@ -13,24 +13,24 @@ const SliderData = [
     image: image2,
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
   },
-  {
-    id: 3,
-    title: "Slide 3",
-    image: "https://www.shutterstock.com/image-photo/business-plan-text-on-modern-260nw-598667390.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
-  },
-  {
-    id: 4,
-    title: "Slide 4",
-    image: "https://img.freepik.com/free-photo/e-learning-class-e-book-digital-technology-education-concept-with-pc-computer_1150-9078.jpg?t=st=1678576099~exp=1678576699~hmac=a4ba58bd93c0d91d64d633210f78c3cf578eac508ce41508d824311aa1c7f18c",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
-  },
-  {
-    id: 5,
-    title: "Slide 5",
-    image: "https://www.shutterstock.com/image-photo/focused-african-business-man-headphones-260nw-1368244235.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
-  },
+  // {
+  //   id: 3,
+  //   title: "Slide 3",
+  //   image: "https://www.shutterstock.com/image-photo/business-plan-text-on-modern-260nw-598667390.jpg",
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Slide 4",
+  //   image: "https://img.freepik.com/free-photo/e-learning-class-e-book-digital-technology-education-concept-with-pc-computer_1150-9078.jpg?t=st=1678576099~exp=1678576699~hmac=a4ba58bd93c0d91d64d633210f78c3cf578eac508ce41508d824311aa1c7f18c",
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Slide 5",
+  //   image: "https://www.shutterstock.com/image-photo/focused-african-business-man-headphones-260nw-1368244235.jpg",
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam ultricies, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+  // },
 ];
 
 export default SliderData;
