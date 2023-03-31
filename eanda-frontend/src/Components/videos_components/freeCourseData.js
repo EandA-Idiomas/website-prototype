1d8f40cd5aca3e0e9fbe825890c500fbe3fae553
pdragoni <paulohbfdf@gmail.com>
2023-03-31 17:47:25 -0300

    [fix] FreeCourse.css, freeCourseData.js and component fixed

---
+++ eanda-frontend/src/Components/videos_components/freeCourseData.js
@@ -0,0 +1,54 @@
+const freeCourse = [
+  {
+    id: 1,
+    title: "Introdução ao Inglês",
+    url: "https://www.youtube.com/watch?v=8hmghEInnps",
+  },
+  {
+    id: 2,
+    title: "Pronomes Simples",
+    url: "https://www.youtube.com/watch?v=62_zH7g1YEQ",
+  },
+  {
+    id: 3,
+    title: "Pronomes de Objeto",
+    url: "https://www.youtube.com/watch?v=GQT2srhxQTo",
+  },
+  {
+    id: 4,
+    title: "Pronomes Possessivos",
+    url: "https://www.youtube.com/watch?v=6qnZ2czLIEo0",
+  },
+  {
+    id: 5,
+    title: "Presente Simples",
+    url: "https://www.youtube.com/watch?v=k4F3TvbCT-w",
+  },
+  {
+    id: 6,
+    title: "Substantivos: Contáveis e Incontáveis - parte 1",
+    url: "https://www.youtube.com/watch?v=QVV85-E7cIM",
+  },
+  {
+    id: 7,
+    title: "Substantivos: Contáveis e Incontáveis - parte 2",
+    url: "https://www.youtube.com/watch?v=Usq61annkBs",
+  },
+  {
+    id: 8,
+    title: "Adjetivos",
+    url: "https://www.youtube.com/watch?v=67JntOkTujw",
+  },
+  {
+    id: 9,
+    title: "Preposições",
+    url: "https://www.youtube.com/watch?v=rpHxhGnxcrw",
+  },
+  {
+    id: 10,
+    title: "Revisão",
+    url: "https://www.youtube.com/watch?v=WHFTL_p4vx4",
+  },
+];
+
+export default freeCourse;
--- eanda-frontend/src/Pages/FreeCourse.jsx
+++ eanda-frontend/src/Pages/FreeCourse.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import ReactPlayer from "react-player";
-import freeCourse from "../Components/videos_components/freeCourse";
+import freeCourseData from "../Components/videos_components/freeCourseData";
+import "../Components/videos_components/freeCourse.css";
 
 function FreeCourse() {
+  const freeCourse = freeCourseData;
+
   if (!freeCourse || freeCourse.length === 0) {
-    return <h3>Curso não encontrado, atualize a página</h3>;
+    return <h3>Desculpe, o curso não pôde ser encontrado.</h3>;
   }
 
   return (
     <div>
       <Header />
-      <h3>Curso gratuito</h3>
-      <section className="free-course-section">
-        {freeCourse.map((freeCourse) => (
-          <div key={freeCourse.id} className="free-course-container">
-            <h4>{freeCourse.title}</h4>
-            <ReactPlayer controls width={520} url={freeCourse?.url} rel={0} />
+      <h2>Curso gratuito</h2>
+      <main className="free-course-main">
+        {freeCourse.map((video) => (
+          <div key={video.id} className="free-course-container">
+            <h3>{video.title}</h3>
+            <ReactPlayer
+              controls
+              width={520}
+              url={video.url}
+              rel={0}
+              alt={`Vídeo de conteúdo ${video.title}`}
+              onError={() => console.log(`Erro ao carregar o vídeo ${video.title}`)}
+            />
           </div>
         ))}
-      </section>
+        <Link to="/">Voltar para a página inicial</Link>
+      </main>
       <Footer />
     </div>
   );
