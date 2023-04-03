20ec0879bf843045f7f3bf143c2440351953898c
pdragoni <paulohbfdf@gmail.com>
2023-04-03 16:55:12 -0300

    [feat] header and footer on Services, content to be added

--- eanda-frontend/src/Pages/Services.jsx
+++ eanda-frontend/src/Pages/Services.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 function Services() {
   return (
     <div>
+      <Header />
       <h1>Services</h1>
       <p>A list of services will go here.</p>
+      <Footer />
     </div>
   );
 }
