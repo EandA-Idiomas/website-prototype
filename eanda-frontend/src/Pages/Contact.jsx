dd6b8b2b3c3f4b49de1332b73bfe6ebbebddd091
pdragoni <paulohbfdf@gmail.com>
2023-04-10 16:24:05 -0300

    [feat] Contact Page and info

--- eanda-frontend/src/Pages/Contact.jsx
+++ eanda-frontend/src/Pages/Contact.jsx
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import WhatsappBanner from "../Components/WhatsappBanner";
+// import WhatsappBanner from "../Components/WhatsappBanner";
+import "../Components/contact_components/Contact.css";
 
 
 function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    window.location.href = `mailto:paulohbfdf@gmail.com?subject=${name}%20-%20Contact%20Form&body=${message}%0D%0A%0D%0AFrom:%20${email}`;
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   window.location.href = `mailto:paulohbfdf@gmail.com?subject=${name}%20-%20Contact%20Form&body=${message}%0D%0A%0D%0AFrom:%20${email}`;
+  // };
 
   return (
     <div>
       <Header />
-      <WhatsappBanner />
+      <body className="contact-section">
+        {/* <h4>Nos contate pelo Whatsapp:</h4>
+      <WhatsappBanner /> */}
+        <div>
+          <h4>Nosso e-mail:</h4>
+          <p>aldaamorim@eanda.com.br</p>
+        </div>
+        <div>
+          <h4>Nosso telefone:</h4>
+          <p>+55 (11) 99959-3752</p>
+        </div>
+        <div className="address">
+          <h4>Nosso endereço:</h4>
+          <p>Rua da Consolação, 2586 - Sala 01</p>
+          <p>Cerqueira César - São Paulo/SP</p>
+          <p>CEP: 58030-043</p>
+        </div>
+        <Link to="/">
+          {/* <img src={image} style={{ height: "250px" }} alt={matcher} /> */}
+          <figcaption>Voltar à Home</figcaption>
+        </Link>
+      </body>
       <Footer />
       {/* <h1>Contact Us</h1>
       <form onSubmit={handleSubmit}>
